9bf7246c1867903be9742ad55dc7a54b9e3cdca4
Fyrezz <eduardo.sanchez.paz@gmail.com>
2019-10-30 18:48:16 +0100

    asd

---
+++ src/main/java/net/fyrezz/equynoxbosses/P.java
@@ -0,0 +1,21 @@
+package net.fyrezz.equynoxbosses;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class P extends JavaPlugin {
+	
+	public static P p;
+	
+	public P() {
+		p = this;
+	}
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+
+}
